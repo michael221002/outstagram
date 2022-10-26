51ec78bc27af746fd21bb0e18ada863bf87af432
michael221002 <114058127+michael221002@users.noreply.github.com>
2022-10-26 10:20:43 +0200

    FIrst Commit

---
+++ frontend/src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  pages = [
+    'page1',
+    'page2',
+    'page3',
+    'page4'
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
